f5e95032a5c45a5e1d64597146fa877936dcede6
wangcaiai <1836523160@qq.com>
2023-03-19 23:40:14 +0800

    Site updated: 2023-03-19 23:40:13

---
+++ 2023/03/19/js复习/restudy.js
@@ -0,0 +1,34 @@
+var str = "abcoefodgpop";
+var index = str.indexOf('o');
+/* var num = 0;
+while (index != -1){
+  console.log(index);
+  num++;
+  index = str.indexOf('o',index + 1);
+} 
+console.log("o出现的次数是" +num); */
+var str = 'aooaooaaabcdo';
+    var n = {};
+    for (var i = 0; i <= str.length; i++){
+      var char = str.charAt(i);
+      if (n [char]){
+        n[char]++;
+      }
+    else{
+      n[char] = 1;
+      }
+    }
+    console.log(n);
+    //遍历对象
+    var max = 0;
+    var ch = '';
+      for (var k in n){
+        //k 是属性名
+        //n[k]是属性值
+      if (n[k] > max){
+        max = n[k];
+        ch = k;
+      }
+}
+    console.log(max);
+    console.log('出现次数最多的是' + ch);
